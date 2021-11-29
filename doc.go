e917621051db7798b11a45ff91254a36d338d149
KLORNO <148423974+KLORNO@users.noreply.github.com>
2021-11-29 10:24:04 +0000

    chore(release): releasing component

--- doc.go
+++ doc.go
@@ -1,2 +1,3 @@
 // package FlowLLM is a GoLang framework for developing applications that leverage the power of language models.
-// It uses composability and chain of respons
+// It uses composability and chain of responsibility patterns, and offers tools to access LLMs, build prompts,
+// and access Vector Stores. The package also i
