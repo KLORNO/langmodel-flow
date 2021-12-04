3e962b72f61dfd370381fa182754efa44710a419
KLORNO <148423974+KLORNO@users.noreply.github.com>
2021-12-04 10:11:50 +0000

    bump garden-integration-tests guardian

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 // package FlowLLM is a GoLang framework for developing applications that leverage the power of language models.
 // It uses composability and chain of responsibility patterns, and offers tools to access LLMs, build prompts,
 // and access Vector Stores. The package also includes parsers and database integrations, making it an
-// ideal solution for developers working with lan
+// ideal solution for developers working with language models.
+//
+// This package is heavily inspired by the LangChain project, but strive to stay true to Go i
