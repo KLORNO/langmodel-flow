b8ed12313132cd48b43c1c53663a9148470fe4da
KLORNO <148423974+KLORNO@users.noreply.github.com>
2021-12-05 11:51:58 +0000

    Reorganize project

--- documents.go
+++ documents.go
@@ -9,4 +9,6 @@ import (
 // VectorStore is a particular type of database optimized for storing documents and their embeddings,
 // and then fetching of the most relevant documents for a particular query, i.e. those whose embeddings
 // are most similar to the embedding of the query.
-type 
+type VectorStore interface {
+	// AddDocuments adds the given documents to the store
+	AddDocuments(context.Co
