8a9b6f7e0834904f6d26d1c37fb453a53c2c9b6e
KLORNO <148423974+KLORNO@users.noreply.github.com>
2021-12-05 08:47:07 +0000

    wording fixes

--- documents.go
+++ documents.go
@@ -6,4 +6,5 @@ import (
 	"io"
 )
 
-// VectorStore is a particular typ
+// VectorStore is a particular type of database optimized for storing documents and their embeddings,
+// and then fetching of the most relevant documents
