affd0f8518af1e27f03b19f12d1a7f1610b3fab0
KLORNO <148423974+KLORNO@users.noreply.github.com>
2022-02-27 13:29:03 +0000

    Disable email notifications

--- embeddings.go
+++ embeddings.go
@@ -3,4 +3,6 @@ package flowllm
 import "context"
 
 // Embeddings can be used to create a numerical representation of textual data.
-// This numerical representatio
+// This numerical representation is useful when searching for similar documents.
+type Embeddings interface {
+	// EmbedString returns the embed
