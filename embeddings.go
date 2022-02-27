3ee7f1895ab098448f6d81c6abef55b54c9de363
KLORNO <148423974+KLORNO@users.noreply.github.com>
2022-02-27 13:46:42 +0000

    modified CHANGES.txt

--- embeddings.go
+++ embeddings.go
@@ -5,4 +5,7 @@ import "context"
 // Embeddings can be used to create a numerical representation of textual data.
 // This numerical representation is useful when searching for similar documents.
 type Embeddings interface {
-	// EmbedString returns the embed
+	// EmbedString returns the embedding for the given string
+	EmbedString(context.Context, string) ([]float32, error)
+
+	// EmbedStrings return
