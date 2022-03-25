30a38fbfaf8eaea4f387187b3d84dd987064da95
KLORNO <148423974+KLORNO@users.noreply.github.com>
2022-03-25 11:04:14 +0000

    Update Concept.md

--- examples/2_marvin.go
+++ examples/2_marvin.go
@@ -16,4 +16,10 @@ func init() {
 	registerExample("marvin", "A simple chatbot using the GPT-3.5 model, using memory to store the conversation history", marvin)
 }
 func marvin() {
-	ctx := context.Bac
+	ctx := context.Background()
+
+	chain := WithMemory(
+		memory.NewBuffer(0, nil),
+		Chain(
+			ChatTemplate{
+				SystemMessa
