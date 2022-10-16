48bfc5696ef636cef7ceeec3d150423b6ef48f1b
KLORNO <148423974+KLORNO@users.noreply.github.com>
2022-10-16 12:30:42 +0000

    setup update

--- integration_tests/vectorstores_integration_test.go
+++ integration_tests/vectorstores_integration_test.go
@@ -137,4 +137,7 @@ var _ = Describe("Vector Stores Integration Tests", func() {
 		},
 		Entry("Memory", func() flowllm.VectorStore { return memoryVS }),
 		Entry("Bolt", func() flowllm.VectorStore { return boltVS }),
-		Entry("Pinecone",
+		Entry("Pinecone", func() flowllm.VectorStore { return pineconeVS }),
+	)
+
+	DescribeTable("It should return an empty resul
