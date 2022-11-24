3d4452d6ec9ad5f3be1ee0a887a011564167799a
KLORNO <148423974+KLORNO@users.noreply.github.com>
2022-11-24 15:52:43 +0000

    change deps

--- llms/openai/openai_chat.go
+++ llms/openai/openai_chat.go
@@ -3,4 +3,10 @@ package openai
 import (
 	"context"
 
-	"github.com/deluan/
+	"github.com/deluan/flowllm"
+	"github.com/sashabaranov/go-openai"
+)
+
+const defaultChatModel = "gpt-3.5-turbo"
+
+// ChatModel is a LLM implementation t
