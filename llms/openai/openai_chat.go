d65066d6d6d2fc7a40fd75c204fe6efc340cc00a
KLORNO <148423974+KLORNO@users.noreply.github.com>
2022-11-30 08:44:43 +0000

    Expand tests

--- llms/openai/openai_chat.go
+++ llms/openai/openai_chat.go
@@ -10,4 +10,5 @@ import (
 const defaultChatModel = "gpt-3.5-turbo"
 
 // ChatModel is a LLM implementation that uses the Chat Completions API with the chat style models, like gpt-3.5-turbo and gpt-4.
-// It uses a spec
+// It uses a special prompt, Chat, to format the messages as expected by the chat completion API.
+// If you use a d
