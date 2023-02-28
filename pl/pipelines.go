fc261aa7fc51d0d772600ebfef626aa365824bfa
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-02-28 16:07:03 +0000

    Add actions

--- pl/pipelines.go
+++ pl/pipelines.go
@@ -29,3 +29,115 @@ func Stage[In any, Out any](
 
 	limit := int64(maxWorkers)
 	sem1 := semaphore.NewWeighted(limit)
+
+	go func() {
+		defer close(outputChannel)
+		defer close(errorChannel)
+
+		for s := range ReadOrDone(ctx, inputChannel) {
+			if err := sem1.Acquire(ctx, 1); err != nil {
+				if !errors.Is(err, context.Canceled) {
+					log.Printf("Failed to acquire semaphore: %v", err)
+				}
+				break
+			}
+
+			go func(s In) {
+				defer sem1.Release(1)
+
+				result, err := fn(ctx, s)
+				if err != nil {
+					if !errors.Is(err, context.Canceled) {
+						errorChannel <- err
+					}
+				} else {
+					outputChannel <- result
+				}
+			}(s)
+		}
+
+		// By using context.Background() here we are assuming the fn will stop when the context
+		// is canceled. This is required so we can wait for the workers to finish and avoid closing
+		// the outputChannel before they are done.
+		if err := sem1.Acquire(context.Background(), limit); err != nil {
+			log.Printf("Failed waiting for workers: %v", err)
+		}
+	}()
+
+	return outputChannel, errorChannel
+}
+
+func Sink[In any](
+	ctx context.Context,
+	maxWorkers int,
+	inputChannel <-chan In,
+	fn func(context.Context, In) error,
+) chan error {
+	results, errC := Stage(ctx, maxWorkers, inputChannel, func(ctx context.Context, in In) (bool, error) {
+		err := fn(ctx, in)
+		return false, err // Only err is important, results will be discarded
+	})
+
+	// Discard results
+	go func() {
+		for range ReadOrDone(ctx, results) {
+		}
+	}()
+
+	return errC
+}
+
+func Merge[T any](ctx context.Context, cs ...<-chan T) <-chan T {
+	var wg sync.WaitGroup
+	out := make(chan T)
+
+	output := func(c <-chan T) {
+		defer wg.Done()
+		for v := range ReadOrDone(ctx, c) {
+			select {
+			case out <- v:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func SendOrDone[T any](ctx context.Context, out chan<- T, v T) {
+	select {
+	case out <- v:
+	case <-ctx.Done():
+		return
+	}
+}
+
+func ReadOrDone[T any](ctx context.Context, in <-chan T) <-chan T {
+	valStream := make(chan T)
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case valStream <- v:
+				case <-ctx.Done():
+				}
+			}
+		}
