6340b28c5de8729bf21192e93c4115698573a629
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-03-01 07:12:17 +0000

    HID: i2c-hid: fix race condition reading reports

--- pl/pipelines.go
+++ pl/pipelines.go
@@ -141,3 +141,37 @@ func ReadOrDone[T any](ctx context.Context, in <-chan T) <-chan T {
 				}
 			}
 		}
+	}()
+	return valStream
+}
+
+func Tee[T any](ctx context.Context, in <-chan T) (<-chan T, <-chan T) {
+	out1 := make(chan T)
+	out2 := make(chan T)
+	go func() {
+		defer close(out1)
+		defer close(out2)
+		for val := range ReadOrDone(ctx, in) {
+			var out1, out2 = out1, out2
+			for i := 0; i < 2; i++ {
+				select {
+				case <-ctx.Done():
+				case out1 <- val:
+					out1 = nil
+				case out2 <- val:
+					out2 = nil
+				}
+			}
+		}
+	}()
+	return out1, out2
+}
+
+func FromSlice[T any](ctx context.Context, in []T) <-chan T {
+	output := make(chan T, len(in))
+	for _, c := range in {
+		output <- c
+	}
+	close(output)
+	return output
+}
---
+++ pl/pipelines_test.go
@@ -0,0 +1,8 @@
+package pl_test
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"testing"
+	"t
