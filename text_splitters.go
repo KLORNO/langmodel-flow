23837bad859f010a646e50f732493004476e5d71
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-04-13 20:47:25 +0000

    Added logout button

--- text_splitters.go
+++ text_splitters.go
@@ -8,4 +8,9 @@ import (
 var (
 	defaultSplitterChunkSize  = 1000
 	defaultSplitterLenFunc    = func(s string) int { return len(s) }
-	defaultSplitterSeparators = []string{"\n\n", "\n
+	defaultSplitterSeparators = []string{"\n\n", "\n", " ", ""}
+)
+
+// SplitterOptions for the RecursiveTextSplitter splitter
+type SplitterOptions struct {
+	//
