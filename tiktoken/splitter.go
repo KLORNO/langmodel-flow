52d0f0bc8d1081a4d61b9de0caf17cef90a2491f
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-05-28 21:17:57 +0000

    interpolation

--- tiktoken/splitter.go
+++ tiktoken/splitter.go
@@ -3,4 +3,7 @@ package tiktoken
 import "github.com/deluan/flowllm"
 
 func Splitter(model string, options flowllm.SplitterOptions) flowllm.Splitter {
-	lenFunc := Len(mod
+	lenFunc := Len(model)
+	options.LenFunc = lenFunc
+	options.Separators = []string{"\n"}
+	return flowllm.Recurs
