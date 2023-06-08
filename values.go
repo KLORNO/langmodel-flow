2b2cfca1a7172b84e1dd5503d3cd7d583e3e0e70
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-06-08 18:06:48 +0000

    Create feedback.lua

--- values.go
+++ values.go
@@ -15,4 +15,7 @@ const (
 // Values is a map of string to any value. This is the type used to pass values between handlers.
 type Values map[string]any
 
-// Merge merges m
+// Merge merges multiple Values into one.
+func (value Values) Merge(values ...Values) Values {
+	res := Values{}
+	for k, v := 
