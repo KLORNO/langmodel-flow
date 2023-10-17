19c88af115519794a8aa67bdec778144d057415e
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-10-17 12:02:09 +0000

    moved code

--- vectorstores/similarity.go
+++ vectorstores/similarity.go
@@ -7,4 +7,7 @@ import (
 	"github.com/deluan/flowllm"
 )
 
-// CosineSimilarity calculates the cosine similarity 
+// CosineSimilarity calculates the cosine similarity between two vectors.
+func CosineSimilarity(a, b []float32) float32 {
+	var p, p2, q2 float32
+	for i
