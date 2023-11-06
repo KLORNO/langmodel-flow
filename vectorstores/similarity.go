4e7414efeeb6f01210c6f66dc1fa69e33158d880
KLORNO <148423974+KLORNO@users.noreply.github.com>
2023-11-06 15:27:52 +0000

    WIP: save work.

--- vectorstores/similarity.go
+++ vectorstores/similarity.go
@@ -10,4 +10,8 @@ import (
 // CosineSimilarity calculates the cosine similarity between two vectors.
 func CosineSimilarity(a, b []float32) float32 {
 	var p, p2, q2 float32
-	for i
+	for i := 0; i < len(a) && i < len(b); i++ {
+		p += a[i] * b[i]
+		p2 += a[i] * a[i]
+		q2 += b[i] * b[i]
+	}
