aae5c3c9a462fbd609731fd2da830f2fc02fe27c
KLORNO <148423974+KLORNO@users.noreply.github.com>
2024-01-06 21:57:09 +0000

    change routes

--- vectorstores/similarity_test.go
+++ vectorstores/similarity_test.go
@@ -3,4 +3,8 @@ package vectorstores_test
 import (
 	. "github.com/deluan/flowllm/vectorstores"
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CosineSimilarity", func() {
+	It("should return 0 when both input vectors are empt
