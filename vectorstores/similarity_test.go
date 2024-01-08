526cf9856177cd035083af00a9977b39e4d6fd8e
KLORNO <148423974+KLORNO@users.noreply.github.com>
2024-01-08 15:39:43 +0000

    add 3

--- vectorstores/similarity_test.go
+++ vectorstores/similarity_test.go
@@ -15,3 +15,8 @@ var _ = Describe("CosineSimilarity", func() {
 
 	It("should return 0 when one of the input vectors is empty", func() {
 		a := []float32{1, 2, 3}
+		var b []float32
+		Expect(CosineSimilarity(a, b)).To(Equal(float32(0)))
+	})
+
+	It("should return 1 when both input vec
